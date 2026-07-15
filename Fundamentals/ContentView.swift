ac185e8fee365c1329f3181f067aafd8efd2af9b
Anthony Santiago <desarrollo.anthony@gmail.com>
2026-07-15 12:42:08 -0400

    Initial Commit

---
+++ Fundamentals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fundamentals
+//
+//  Created by Anthony on 15/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Anthony on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
